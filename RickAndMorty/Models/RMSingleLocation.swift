74211b773171016fa10e3f092e0559c510987a51
Đỗ Minh Quân <minhquan.5322@aol.com>
2023-01-13 11:36:39 +0700

    implement some data model

---
+++ RickAndMorty/Models/RMSingleLocation.swift
@@ -0,0 +1,13 @@
+//
+//  RMSingleLocation.swift
+//  RickAndMorty
+//
+//  Created by Minh Quan on 13/01/2023.
+//
+
+import Foundation
+
+struct RMSingleLocation: Codable {
+    let name: String
+    let url: String 
+}

cf03cf4383ef4cc54486729780e9c85ecbcbab46
Đỗ Minh Quân <minhquan.5322@aol.com>
2023-01-15 15:41:42 +0700

    Add API called

--- RickAndMorty/Controllers/Core/RMCharacterViewController.swift
+++ RickAndMorty/Controllers/Core/RMCharacterViewController.swift
@@ -16,13 +16,18 @@ final class RMCharacterViewController: UIViewController {
             endpoint: .character,
             queryParameters: [
                 URLQueryItem(name: "name", value: "rick"),
-                URLQueryItem(name: "statuc", value: "alive")
+                URLQueryItem(name: "status", value: "alive")
             ]
         )
         print(request.url as Any)
         
-        RMService.shared.execute(request, expecting: RMCharacter.self) { result in
-            
+        RMService.shared.execute(.listCharactersRequests, expecting: RMGetAllCharactersResonse.self) { result in
+            switch result {
+            case .success(let model):
+                print(String(describing: model))
+            case .failure(let error):
+                print(String(describing: error))
+            }
         }
     }
 }
